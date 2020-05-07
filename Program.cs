14e2eed83d42d5e7b67a67c343437e0f07a7d891
Maurício <mauriciobastiani@outlook.com>
2020-05-07 18:42:15 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace api_key_generator
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Quantidades de chaves que deseja gerar? (default 1)");
+            var line = Console.ReadLine();
+            int quantidade = Convert.ToInt32(string.IsNullOrWhiteSpace(line)? "1" : line);
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                var key = new byte[64];
+                using (var generator = RandomNumberGenerator.Create())
+                    generator.GetBytes(key);
+
+                Console.WriteLine();
+                Console.WriteLine(Convert.ToBase64String(key));
+            }
+            Console.ReadKey();
+        }
+    }
+}
